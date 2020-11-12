8dd528ef83defcdb9ad2b1acdd7de9c204aac6e7
MarkVarga <67153681+MarkVarga@users.noreply.github.com>
2020-11-12 11:33:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+'use strict';
+//The number to guess
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+//Player score
+let score = 20;
+//Highscore
+let highscore = 0;
+//Display message function
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    //When there is no input
+    displayMessage('No number!');
+  } else if (guess === secretNumber) {
+    //When player wins
+    displayMessage('Correct number!');
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = secretNumber;
+    //Highscore
+    if (score > highscore) {
+      highscore = score;
+
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    //When guess is wrong
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      //When player loses the game
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+//Again button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.number').style.width = '15rem';
+});
